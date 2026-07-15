85e9a1642802cd8cc8d70e6336ddee56ba5433dc
hakimcolor <hakimcolor777@gmail.com>
2026-07-15 11:13:39 +0600

    add context

---
+++ src/context/CartProvider.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([])
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartProvider
