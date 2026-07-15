43ca5f8a6996c4bb4c5bffeb49671b2b037c0f2c
hakimcolor <hakimcolor777@gmail.com>
2026-07-15 11:28:15 +0600

    defind the outlet in rootlayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Naveber from '../Components/Naveber'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer'
 
 const RootLayout = () => {
   return (
     <div>
-      
+      <Naveber />
+      <Outlet />
+      <Footer/>
     </div>
   )
 }
