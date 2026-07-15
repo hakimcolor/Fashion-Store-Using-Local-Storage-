4d4bbe01adfefd2f3c9f6b50ddf129cdb4d91ce0
hakimcolor <hakimcolor777@gmail.com>
2026-07-15 11:22:12 +0600

    i add router , footer , navber in router and components

---
+++ src/Components/Naveber.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Naveber = () => {
+  return (
+    <div>
+      Header.............
+    </div>
+  )
+}
+
+export default Naveber
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
+import router from './routers/Router.jsx'
+import CartProvider from './context/CartProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
