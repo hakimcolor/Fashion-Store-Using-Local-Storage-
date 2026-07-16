7ed34e82fa1b4f980ff73d2f5ca8a6479759065c
hakimcolor <hakimcolor777@gmail.com>
2026-07-16 12:35:17 +0600

    i add logo and product icon

--- src/Components/Naveber.jsx
+++ src/Components/Naveber.jsx
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 const Naveber = () => {
   const [open, setOpen] = useState(false);
   return (
-    <header>
-      <div>
-        <div>
-          <Link to="/">
-            OXI <span>STYLE</span>
+    <header className="sticky top-0 z-50 bg-white/80 backdrop-blur-lg border-b border-gray-200">
+      <div className="max-w-7xl mx-auto px-5">
+        <div className="flex items-center justify-between h-20">
+          {/* Logo and Project icon  */}
+          <Link to="/" className="text-3xl font-black tracking-wide">
+            OXI<span className="text-gray-400">STYLE</span>
           </Link>
+
+
+
+
         </div>
       </div>
     </header>
