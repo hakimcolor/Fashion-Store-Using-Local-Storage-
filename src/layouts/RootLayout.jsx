2ecdfbed55c1bae751e013ece3dfbc1f2f6c840f
hakimcolor <hakimcolor777@gmail.com>
2026-07-17 20:40:09 +0600

    add loder

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Naveber from '../Components/Naveber';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import ScrollToTop from '../Components/ScrollToTop';
+import Loader from '../Components/Loader';
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
       <ScrollToTop />
+      {navigation.state === 'loading' && <Loader />}
       <Naveber />
       <Outlet />
       <Footer />
