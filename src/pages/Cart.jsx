dfbfd4e0960aa0ebb75bbc468184f525232e7f60
hakimcolor <hakimcolor777@gmail.com>
2026-07-17 09:17:28 +0600

    add a handel remove for deleat item inlocal storage

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,10 +5,35 @@ const Cart = () => {
   const { cart } = useContext(CartContext);
 
   console.log(cart);
-  return <div>it's cart page ............
-
-    
-  </div>;
+  return (
+    <div>
+      <h1>Shopping cart</h1>
+      {cart.length === 0 ? (
+        <div>
+          <h2>Your Cart is Empty</h2>
+        </div>
+      ) : (
+        <div>
+          <div>
+            {cart.map((item) => (
+              <div key={item.id}>
+                {/* leftsite */}
+                <div>
+                  <img src={item.image} alt={item.name} />
+                  <div>
+                    <h2>{item.name}</h2>
+                    <p>Cattgory:{item.category}</p>
+                    <p>Color:{item.selectedColor}</p>
+                    <p>size:{item.selectedSize }</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
