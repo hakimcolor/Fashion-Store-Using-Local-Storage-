ff1f509c544190e4e1ea36c8a0d3c1514476b4c7
hakimcolor <hakimcolor777@gmail.com>
2026-07-17 20:42:20 +0600

    remove all commetn cod

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,18 +1,4 @@
-// import React from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import CartStyle from '../Components/cartStyle';
 
-// const Product = () => {
-//   const products = useLoaderData();
-//   console.log(products);
-//   return (
-//     <div className="max-w-[90%] mx-auto">
-//       <CartStyle products={products} />
-//     </div>
-//   );
-// };
-
-// export default Product;
 
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
