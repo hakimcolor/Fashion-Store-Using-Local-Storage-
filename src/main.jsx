f344ad479e99b949dff2de7823bc809d060a8eb3
hakimcolor <hakimcolor777@gmail.com>
2026-07-17 16:20:13 +0600

    install aso for for animation

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,17 @@ import { Toaster } from 'react-hot-toast';
 import router from './routers/Router.jsx';
 import CartProvider from './context/CartProvider.jsx';
 
+
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// Initialize AOS
+AOS.init({
+  duration: 1000, // 1 second animation
+  once: true,
+  easing: 'ease-out',
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartProvider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import HomeSlider from '../Components/HomeSlider';
 import { useLoaderData } from 'react-router-dom';
+import CartStyle from '../Components/CartStyle';
 
 const Home = () => {
     const products = useLoaderData();
 
   return (
     <div>
-    
       <HomeSlider />
 
-      <div className='mt-100'>dlsfl;dskfl;</div>
-      
+      <div className="mt-20 max-w-[90%] mx-auto ">
+        <h1 className="text-4xl font-bold text-center mb-8 arbutus-slab">
+          Featured Products
+        </h1>
+
+        <CartStyle products={products.slice(0, 4)} />
+      </div>
     </div>
   );
 };
