898275a9e8123c67cd9886344fd36a5282d614f8
hakimcolor <hakimcolor777@gmail.com>
2026-09-15 21:56:26 +0600

    style: loader text dark mode color added

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -13,7 +13,9 @@ const Loader = () => (
     <p className="arbutus-slab text-2xl font-bold tracking-wide dark:text-white">
       OXI<span style={{ color: '#8FA28A' }}>STYLE</span>
     </p>
-    <p className="dmsans text-sm text-gray-400">loading, please wait... ✨</p>
+    <p className="dmsans text-sm text-gray-400 dark:text-gray-500">
+      loading, please wait... ✨
+    </p>
   </div>
 );
 
